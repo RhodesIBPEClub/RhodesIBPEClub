a7b2542ddc33a50c5d5002a7cdb0b5d6b2c048d5
Turner Naef <Turner.Naef@gmail.com>
2023-06-17 12:29:08 -0600

    Changing base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RhodesIBPEClub/assets/", // for github pages
+  base: "/RhodesIBPEClub/", // for github pages
   plugins: [react()],
 })
