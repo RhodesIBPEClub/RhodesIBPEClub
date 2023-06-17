05fd4ce1966a9b89bc137dfa6b3cebffaa40176d
Turner Naef <Turner.Naef@gmail.com>
2023-06-17 10:47:15 -0600

    fix for deploy through pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RhodesIBPEClub/", // for github pages
+  base: "/RhodesIBPEClub.github.io/", // for github pages
   plugins: [react()],
 })
