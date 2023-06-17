ea39414e5aea3b73f2189324b15324a8527e3795
Turner Naef <Turner.Naef@gmail.com>
2023-06-17 11:51:23 -0600

    Rename repo and fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RhodesIBPEClub.github.io/", // for github pages
+  base: "/rhodesibpeclub/", // for github pages
   plugins: [react()],
 })
